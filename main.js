45e7e873f1860e5e600e7741ac5ea7040b25cf09
KazutoRiyama <brianbluchet@hotmail.fr>
2026-02-03 22:46:37 +0100

    fixed

--- main.js
+++ main.js
@@ -14,9 +14,15 @@ const context = {
         },
         {
             lnimage: 'resources/images/OtonariAsobi.jpg',
-            JPtitle: 'orfkeojefp',
-            roumaji: 'a definir',
-            synopsis: `WIP`
+            JPtitle: '迷子になっていた幼女を助けたら、お隣に住む美少女留学生が家に遊びに来るようになった件について',
+            roumaji: '(Maigo ni natte ita youjo wo tasuketara, otonari ni sumu bishoujo ryuugakusei ga ie ni asobi ni kuruyou ni natta ken ni tsuite)',
+            synopsis: `One day, a foreign exchange student was transfered in Aoyagi Akito's class. Her name is Charlotte Bennett. Everyone is 
+            immediately attracted by a pure and innocent personnality, her refined demeanor and her cuteness. But, with calm, Akito just think 'we live in different worlds'
+            and stay away. However, when he helps his little sister Emma who was lost, Akito's life took a dramatic turns.
+            Akito finds out that the Bennet sisters live in the appartment next door, and because Emma took a liking to him,
+            they suddenly started coming over in his appartment almost every day. The three of them play, eat dinner and go out together.
+            As they spend time together, Akito and Charlotte naturally grow close, albeit akwardly. - the start of a sweet
+            and frustrating love comedy next door!`
         }
     ]
 };
