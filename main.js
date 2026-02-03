ffe41f8bdab806ca8f7ff2535d2a9a85e4c00b63
KazutoRiyama <brianbluchet@hotmail.fr>
2026-02-03 23:01:18 +0100

    added third LN

--- main.js
+++ main.js
@@ -23,6 +23,31 @@ const context = {
             they suddenly started coming over in his appartment almost every day. The three of them play, eat dinner and go out together.
             As they spend time together, Akito and Charlotte naturally grow close, albeit akwardly. - the start of a sweet
             and frustrating love comedy next door!`
+        },
+        {
+        lnimage: 'resources/images/MakeHeroin.jpg',
+        JPtitle: '負けヒロインが多すぎる！',
+        roumaji: '(Make hiroin ga oosugiru!)',
+        synopsis: `「え？ マケインって誰のこと？」
+
+                    クラスの背景である俺――温水和彦は、あるとき人気女子・八奈見杏菜が男子に振られるのを目撃する。
+
+                    「私をお嫁さんにするって言ったのに、ひどくないかな？」
+                    「それ、いくつの頃の話？」
+                    「４、５歳だけど」
+
+                    それはノーカンだろ。
+                    これをきっかけに、陸上部の焼塩檸檬、文芸部の小鞠知花など、負け感あふれる女子たちが現れて――？
+
+                    「温水君。女の子は２種類に分けられるの。幼馴染か、泥棒猫か」
+                    「なるほど、大胆な分類だ」
+
+                    負けてこそ輝く彼女たちに、幸いあれ。
+                    負けヒロイン――マケインたちに絡まれる謎の青春が、ここに幕を開ける！
+
+                    ※「ガ報」付き！
+
+                    ※この作品は底本と同じクオリティのカラーイラスト、モノクロの挿絵イラストが収録されています。`
         }
     ]
 };
